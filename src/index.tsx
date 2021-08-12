f2be7a74f5472dc69ed5d71a0e8028e4383d565f
Alexander Radyushin <r.alex.andr@ya.ru>
2021-08-13 00:22:32 +0300

    chore(deps): used fetch polyfill instead of ponyfill

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import { useState, useCallback, useMemo, useEffect } from 'react';
 import * as PropTypes from 'prop-types';
-import fetch from 'cross-fetch';
+import 'cross-fetch/polyfill';
 import { DocumentNode, OperationDefinitionNode } from 'graphql';
 // Apollo client library
 // eslint-disable-next-line import/no-extraneous-dependencies
